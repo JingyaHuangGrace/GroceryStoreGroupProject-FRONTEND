9cd9a33ddcb73bee948cef77cae3a8d42dce5faa
Jingya Huang <jhuan180@my.centennialcollege.ca>
2023-08-04 23:12:51 -0400

    modified baseUrl in product, category and contact service file

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -3,7 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Category } from '../models/category.model';
 
-const baseUrl = 'http://localhost:8079/api/categories';
+//const baseUrl = 'http://localhost:8079/api/categories';
+const baseUrl = 'https://team-unite-grocerystore.onrender.com/api/categories';
 
 @Injectable({
   providedIn: 'root'
